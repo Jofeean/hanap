bbdecb49a787573de3185f015fb1a1679f26e6be
Jofeean Ogario <kintot24@gmail.com>
2017-12-20 03:12:34 +0800

    list gallery

--- resources/views/missing/list.blade.php
+++ resources/views/missing/list.blade.php
@@ -58,7 +58,7 @@
                                         }
                                         ?>
 
-                                        @if($i == 6 || $loop->last)
+                                        @if($i == 6 || $loop->parent->last)
                                             <div class="carousel-item @if($t == 1) active @endif">
                                                 <div style="margin: auto">
                                                     <table style="height: 370px;">
