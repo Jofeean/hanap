316dbee84e828bc1670cbb1126c7ee4bec7d54ea
Jofeean Ogario <kintot24@gmail.com>
2017-12-20 04:21:17 +0800

    gallery admin

--- resources/views/admin/missings.blade.php
+++ resources/views/admin/missings.blade.php
@@ -17,41 +17,6 @@
 
     <div class="content">
         <div class="container-fluid">
-            <div class="row">
-                <div id="myCarousel" class="carousel slide" data-ride="carousel">
-                    <!-- Indicators -->
-                    <ol class="carousel-indicators">
-                        <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-                        <li data-target="#myCarousel" data-slide-to="1"></li>
-                        <li data-target="#myCarousel" data-slide-to="2"></li>
-                    </ol>
-
-                    <!-- Wrapper for slides -->
-                    <div class="carousel-inner">
-                        <div class="item active">
-                            <img src="la.jpg" alt="Los Angeles">
-                        </div>
-
-                        <div class="item">
-                            <img src="chicago.jpg" alt="Chicago">
-                        </div>
-
-                        <div class="item">
-                            <img src="ny.jpg" alt="New York">
-                        </div>
-                    </div>
-
-                    <!-- Left and right controls -->
-                    <a class="left carousel-control" href="#myCarousel" data-slide="prev">
-                        <span class="glyphicon glyphicon-chevron-left"></span>
-                        <span class="sr-only">Previous</span>
-                    </a>
-                    <a class="right carousel-control" href="#myCarousel" data-slide="next">
-                        <span class="glyphicon glyphicon-chevron-right"></span>
-                        <span class="sr-only">Next</span>
-                    </a>
-                </div>
-            </div>
 
             <div class="row">
                 <div class="col-md-12">
@@ -149,12 +114,12 @@
                                     </div>
                                     <a class="left carousel-control"
                                        href="#carouselExampleIndicators-1" data-slide="prev">
-                                        <span class="glyphicon glyphicon-chevron-left"></span>
+                                        <span class="ti-angle-left"></span>
                                         <span class="sr-only">Previous</span>
                                     </a>
                                     <a class="right carousel-control"
                                        href="#carouselExampleIndicators-1" data-slide="next">
-                                        <span class="glyphicon glyphicon-chevron-right"></span>
+                                        <span class="ti-angle-right"></span>
                                         <span class="sr-only">Next</span>
                                     </a>
                                 </div>
