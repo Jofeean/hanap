e3ca3c106f611b8f119518129501821c12ea9bbd
Jofeean Ogario <kintot24@gmail.com>
2017-12-20 01:52:27 +0800

    list gallery

--- resources/views/missing/list.blade.php
+++ resources/views/missing/list.blade.php
@@ -61,12 +61,43 @@
                                                     <table style="height: 500px;">
                                         @endif
 
-                                                    <td valign="middle" align="center">
-                                                        <img class="d-block img-fluid"
-                                                             src="{!! asset('images/missingthumb/'. $gallery->Missing_picture) !!}"
-                                                             alt="First slide"
-                                                             style="max-width: 100%; max-height: 500px; width: auto;">
-                                                    </td>
+                                                        <td valign="middle" align="center">
+                                                            <img class="d-block img-fluid"
+                                                                 src="{!! asset('images/missingthumb/'. $gallery->Missing_picture) !!}"
+                                                                 alt="First slide"
+                                                                 style="max-width: 100%; max-height: 500px; width: auto;">
+                                                        </td>
+                                                        <td valign="middle" align="center">
+                                                            <img class="d-block img-fluid"
+                                                                 src="{!! asset('images/missingthumb/'. $gallery->Missing_picture) !!}"
+                                                                 alt="First slide"
+                                                                 style="max-width: 100%; max-height: 500px; width: auto;">
+                                                        </td>
+                                                        <td valign="middle" align="center">
+                                                            <img class="d-block img-fluid"
+                                                                 src="{!! asset('images/missingthumb/'. $gallery->Missing_picture) !!}"
+                                                                 alt="First slide"
+                                                                 style="max-width: 100%; max-height: 500px; width: auto;">
+                                                        </td>
+                                                        <td valign="middle" align="center">
+                                                            <img class="d-block img-fluid"
+                                                                 src="{!! asset('images/missingthumb/'. $gallery->Missing_picture) !!}"
+                                                                 alt="First slide"
+                                                                 style="max-width: 100%; max-height: 500px; width: auto;">
+                                                        </td>
+                                                        <td valign="middle" align="center">
+                                                            <img class="d-block img-fluid"
+                                                                 src="{!! asset('images/missingthumb/'. $gallery->Missing_picture) !!}"
+                                                                 alt="First slide"
+                                                                 style="max-width: 100%; max-height: 500px; width: auto;">
+                                                        </td>
+                                                        <td valign="middle" align="center">
+                                                            <img class="d-block img-fluid"
+                                                                 src="{!! asset('images/missingthumb/'. $gallery->Missing_picture) !!}"
+                                                                 alt="First slide"
+                                                                 style="max-width: 100%; max-height: 500px; width: auto;">
+                                                        </td>
+
 
                                         @if($i == 0)
                                                 </table>
@@ -75,6 +106,26 @@
                                             </div>
                                             <div class="carousel-caption">
                                         @endif
+                                                <div class="col-md-2"
+                                                     style="background: rgba(11,16,17,0.21); border-radius: 10px">
+                                                    qer
+                                                </div>
+                                                <div class="col-md-2"
+                                                     style="background: rgba(11,16,17,0.21); border-radius: 10px">
+                                                    qer
+                                                </div>
+                                                <div class="col-md-2"
+                                                     style="background: rgba(11,16,17,0.21); border-radius: 10px">
+                                                    qer
+                                                </div>
+                                                <div class="col-md-2"
+                                                     style="background: rgba(11,16,17,0.21); border-radius: 10px">
+                                                    qer
+                                                </div>
+                                                <div class="col-md-2"
+                                                     style="background: rgba(11,16,17,0.21); border-radius: 10px">
+                                                    qer
+                                                </div>
                                                 <div class="col-md-2"
                                                      style="background: rgba(11,16,17,0.21); border-radius: 10px">
                                                     qer
