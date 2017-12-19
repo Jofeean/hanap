ecf31cbf6d783070621e9b45d52e3596ca0b64ca
Jofeean Ogario <kintot24@gmail.com>
2017-12-20 01:39:04 +0800

    list gallery

--- routes/web.php
+++ routes/web.php
@@ -52,7 +52,7 @@ Route::get('/missingperson/reportsighting', function () {
 Route::get('/missingperson/list', function () {
     $users = new App\missing;
     $data['missings'] = $users->where('Missing_status', '=', '0')->paginate(15);
-    $data['galleries'] = $users->where('Missing_status', '=', '0')->inRandomOrder()->limit(10)->get();
+    $data['galleries'] = $users->where('Missing_status', '=', '0')->inRandomOrder()->limit(30)->get();
     $users = new App\user;
     $data['users'] = $users->get();
 
